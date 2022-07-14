d424d734ba8cb61751769572d9e9f57aeff14ea3
luk <luk@funkelab-ws4.hhmi.org>
2022-07-14 00:44:59 -0400

    add functions that generate bounding boxes

--- dnmfx/synthesize.py
+++ dnmfx/synthesize.py
@@ -2,6 +2,7 @@ import numpy as np
 import zarr
 import random
 import os
+import cv2
 from sklearn.datasets import make_blobs
 
 
@@ -48,65 +49,77 @@ def synthesize(image_size, cell_size, num_cells, num_frames, random_seeds,
     write_object_to_zarr(parameters.ground_truth_data, "centers", centers)
 
     traces = generate_traces(parameters)
-    write_object_to_zarr(parameters.ground_truth_data, "traces", traces)
+    save_cell_traces(parameters, traces)
 
     cell_coordinates = generate_cells(parameters)
     save_cell_centers(parameters)
     save_cell_components(parameters, cell_coordinates)
 
+    cell_patch_image, bounding_boxes, cell_centers = generate_cell_patch(
+                                                            parameters,
+                                                            cell_coordinates)
+    save_cell_patch(parameters, cell_patch_image, bounding_boxes, cell_centers)
+
     background = generate_background(parameters)
     background_reshaped = background.reshape(parameters.image_size,
                                              parameters.image_size)
-    write_object_to_zarr(parameters.ground_truth_data, "background",
-                         background_reshaped)
+    write_object_to_zarr(parameters.ground_truth_data, "background.zarr",
+                         zarr.array(background_reshaped))
 
-    cells = zarr.group(store=f"{parameters.data_path}/cells", overwrite=True)
-    ensemble = zarr.group(store=f"{parameters.data_path}/ensemble",
-                          overwrite=True)
+    cell_group = zarr.group(store=f"{parameters.data_path}/cells",
+                            overwrite=True)
+    ensemble_group = zarr.group(store=f"{parameters.data_path}/ensemble",
+                                overwrite=True)
+    noise_group = zarr.group(store=f"{parameters.data_path}/noises",
+                             overwrite=True)
     ensemble_sequence, cell_sequence = assemble_sequence(parameters, traces,
                                                          cell_coordinates,
-                                                         background, ensemble,
-                                                         cells)
-    write_object_to_zarr(ensemble, "sequence", ensemble_sequence)
-    write_object_to_zarr(cells, "sequence", cell_sequence)
+                                                         background,
+                                                         ensemble_group,
+                                                         cell_group,
+                                                         noise_group)
+    write_object_to_zarr(ensemble_group, "sequence.zarr", ensemble_sequence)
+    write_object_to_zarr(cell_group, "sequence.zarr", cell_sequence)
 
 
 def make_directories(parameters):
     '''
     Here shows the Zarr groups to be created for storing our Synthetic Dataset:
     └── reconstruction/ or ground_truth/
-        ├── background/
+        ├── background.zarr/
         ├── cells/
-        ├── centers/
+        ├── centers.zarr/
         ├── noises/
         ├── components/
         ├── traces/
         └── ensemble/
     '''
 
-    object_names = ['background', 'cells', 'centers', 'noises', 'components',
-                    'traces', 'ensemble']
+    object_names = ['background.zarr', 'cells', 'centers.zarr', 'noises',
+                    'components', 'traces', 'ensemble']
 
-    reconstruction_path = f"{parameters.home_path}/reconstruction"
     parameters.data_path = f"{parameters.home_path}/ground_truth"
-
     ground_truth_store = zarr.DirectoryStore(parameters.data_path)
-    reconstruction_store = zarr.DirectoryStore(reconstruction_path)
     parameters.ground_truth_data = zarr.group(store=ground_truth_store,
                                               overwrite=True)
-    parameters.reconstructed_data = zarr.group(store=reconstruction_store,
-                                               overwrite=True)
-
     for object_name in object_names:
         parameters.ground_truth_data.create_group(object_name)
-        parameters.reconstructed_data.create_group(object_name)
 
+
+def save_cell_traces(parameters, traces):
+
+    trace_group = zarr.group(store=f"{parameters.data_path}/traces",
+                             overwrite=True)
+    for i in range(parameters.num_cells):
+        write_object_to_zarr(trace_group, f"trace{i}.zarr",
+                             zarr.array(traces[i, :]))
 
 def save_cell_centers(parameters):
 
     cell_centers = np.array([(r*parameters.cell_size, c*parameters.cell_size)
                             for (r, c) in parameters.cell_centers])
-    write_object_to_zarr(parameters.ground_truth_data, "centers", cell_centers)
+    write_object_to_zarr(parameters.ground_truth_data, "centers.zarr",
+            cell_centers)
     print('cell centers saved!')
 
 
@@ -121,13 +134,13 @@ def save_cell_components(parameters, cell_coordinates):
         np.put(cell_component, cell_indices, [255]*len(cell_indices))
         cell_component = cell_component.reshape(parameters.image_size,
                                                 parameters.image_size)
-        write_object_to_zarr(components, f"component{ci}",
+        write_object_to_zarr(components, f"component{ci}.zarr",
                              zarr.array(cell_component))
         print(f'cell frame{ci}.zarr saved')
 
 
 def assemble_sequence(parameters, traces, cell_coordinates, background,
-                      ensemble, cells):
+                      ensemble_group, cell_group, noise_group):
 
     ensemble_sequence = np.zeros((parameters.num_frames, parameters.image_size,
                                   parameters.image_size))
@@ -136,27 +149,34 @@ def assemble_sequence(parameters, traces, cell_coordinates, background,
 
     # assemble cell at each frame
     for current_frame in range(parameters.num_frames):
-        ensemble_frame, cell_frame = assemble_frame(parameters, traces,
-                                                    cell_coordinates,
-                                                    current_frame, background,
-                                                    cells, ensemble)
+        ensemble_frame, cell_frame, noise_frame = assemble_frame(
+                                                        parameters,
+                                                        traces,
+                                                        cell_coordinates,
+                                                        current_frame,
+                                                        background)
         cell_frame = cell_frame.reshape(parameters.image_size,
                                         parameters.image_size)
         ensemble_frame = ensemble_frame.reshape(parameters.image_size,
                                                 parameters.image_size)
-        write_object_to_zarr(cells, f"frame{current_frame}",
+        noise_frame = noise_frame.reshape(parameters.image_size,
+                                          parameters.image_size)
+
+        write_object_to_zarr(cell_group, f"frame{current_frame}.zarr",
                              zarr.array(cell_frame))
-        write_object_to_zarr(ensemble, f"frame{current_frame}",
+        write_object_to_zarr(ensemble_group, f"frame{current_frame}.zarr",
                              zarr.array(ensemble_frame))
+        write_object_to_zarr(noise_group, f"frame{current_frame}.zarr",
+                             zarr.array(noise_frame))
         print(f"frame {current_frame} assembled")
-        ensemble_sequence[current_frame, :, :] = zarr.array(ensemble_frame)
-        cell_sequence[current_frame, :, :] = zarr.array(cell_frame)
+        ensemble_sequence[current_frame, :, :] = ensemble_frame
+        cell_sequence[current_frame, :, :] = cell_frame
 
     return ensemble_sequence, cell_sequence
 
 
 def assemble_frame(parameters, traces, cell_coordinates, current_frame,
-                   background, cells, ensemble):
+                   background):
 
     cell_intensities = traces[:, current_frame]
     ensemble_frame = np.copy(background)
@@ -170,13 +190,13 @@ def assemble_frame(parameters, traces, cell_coordinates, current_frame,
         # create calcium image
         ensemble_frame[cell_indices] += color
 
-    extra_noise = np.array([abs(np.random.normal(10,
+    noise_frame = np.array([abs(np.random.normal(10,
                            parameters.extra_noise_mag)) for b in
                            ensemble_frame])
-    ensemble_frame = np.array([ensemble_frame[i] + extra_noise[i] for i in
+    ensemble_frame = np.array([ensemble_frame[i] + noise_frame[i] for i in
                                range(parameters.image_size**2)])
 
-    return ensemble_frame, cell_frame
+    return ensemble_frame, cell_frame, noise_frame
 
 
 def generate_background(parameters):
@@ -229,6 +249,49 @@ def generate_cells(parameters):
 
     return all_cell_coordinates
 
+
+def generate_cell_patch(parameters, all_cell_coordinates):
+
+    cell_patches = np.zeros(parameters.image_size**2)
+    bounding_boxes = []
+    cell_centers = []
+
+    for cell_coordinates in all_cell_coordinates:
+        x_begin = int(min(list(zip(*cell_coordinates))[0]))
+        x_end = min(x_begin + parameters.cell_size, parameters.image_size-1)
+        y_begin = int(min(list(zip(*cell_coordinates))[1]))
+        y_end = min(y_begin + parameters.cell_size, parameters.image_size-1)
+        bounding_boxes.append((x_begin, x_end, y_begin, y_end))
+        cell_patch_coordinates = [(x, y) for x in range(x_begin, x_end+1) for y
+                                 in range(y_begin, y_end)]
+        cell_patch_indices = [int(i*parameters.image_size+j) for i, j in
+                             cell_patch_coordinates]
+        cell_centers.append(get_cell_centers(parameters, cell_patch_indices))
+        np.put(cell_patches, cell_patch_indices, [256]*len(cell_patch_indices))
+
+    return cell_patches, bounding_boxes, cell_centers
+
+def get_cell_centers(parameters, cell_patch_indices):
+    image = np.zeros(parameters.image_size**2)
+    np.put(image, cell_patch_indices, [256]*len(cell_patch_indices))
+    moments = cv2.moments(image.reshape(parameters.image_size,
+                                        parameters.image_size))
+    x = int(moments["m10"] / moments["m00"])
+    y = int(moments["m01"] / moments["m00"])
+
+    return (x,y)
+
+
+def save_cell_patch(parameters, cell_patch_image, bounding_boxes, cell_centers):
+
+    cell_patch = cell_patch_image.reshape(parameters.image_size,
+                                          parameters.image_size)
+    cv2.imwrite('cell_patch.jpg', cell_patch)
+    cell_patch = zarr.array(cell_patch_image.reshape(parameters.image_size,
+                            parameters.image_size))
+    zarr.save("cell_patches.zarr", cell_patch)
+    np.save("bounding_boxes.npy", bounding_boxes)
+    np.save("cell_centers.npy", cell_centers)
 
 def write_object_to_zarr(zarr_group, object_name, object_data):
     zarr_group[object_name] = object_data
