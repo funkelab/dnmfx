c66911c5421ac2188332b3e7ca61b2ddbf227f61
Alicia Lu <lualicia88@gmail.com>
2022-07-14 17:12:53 -0400

    Add script that creates components given bounding boxes as funlib.geometry.ROI

--- dnmfx/preprocess.py
+++ dnmfx/preprocess.py
@@ -1,120 +1,125 @@
-import numpy as np
-import jax
-import cv2
-import random
-import optax
 from scipy import spatial
+import math
+import funlib.geometry as fg
+import numpy as np
 
 class Component():
+    '''Represents a single component by its bounding box in the volume and a
+    unique index.
 
-    def __init__(self, bounding_box, box_index):
-        # with the 2D dataset
-        # bounding_box = (x_begin, x_end, y_begin, y_end)
-        # begin = (x_begin, y_begin);
-        # end = (x_end, y_end)
-        self.begin = (bounding_box[0], bounding_box[2])
-        self.end = (bounding_box[1], bounding_box[3])
-        self.index = box_index
+    Args:
+
+        bounding_box (:class:`funlib.geometry.Roi`):
+            The bounding box of the component, in voxels.
+
+        component_index (int):
+            A unique zero-based and continuous index for each component.
+    '''
+
+    def __init__(self, bounding_box, component_index):
+
+        self.bounding_box = bounding_box
+        self.index = component_index
+        self.overlapping_components = []
 
 
-def create_overlap_lookup_dict(bounding_boxes):
+def create_components(bounding_boxes):
+    '''Create a list of components from bounding boxes.
 
+    Args:
+
+        bounding_boxes (list of :class:`funlib.geometry.Roi`):
+            A list of bounding boxes.
+
+    Returns:
+
+        A list of :class:`Component`, where each component stores a list of
+        other components it overlaps with.
+    '''
+
+    # create components from bounding boxes
     components = construct_components(bounding_boxes)
+
+    # get all components' centers
+    component_centers = [c.bounding_box.center for c in components]
+
+    # create a KD-Tree
+    kd_tree = spatial.KDTree(component_centers)
+
+    # store overlapping components
     overlaps = {
-                    component.index: find_overlapping_components(component,
-                                     components) for component in components
-               }
+        component.index:
+        find_overlapping_components(component, components, kd_tree)
+        for component in components
+    }
 
-    return overlaps
+    for index, overlapping_components in overlaps.items():
+        components[index].overlapping_components = overlapping_components
+
+    return components
 
 
-def find_overlapping_components(component, components):
+def find_overlapping_components(component, components, kd_tree):
 
+    component_center = component.bounding_box.center
+
+    # query all components that are close to 'component'
+    close_component_indices = kd_tree.query_ball_point(
+        component_center,
+        get_diagonal_length(component) + 1
+    )
+    close_components = [
+        components[i]
+        for i in close_component_indices
+    ]
+
+    # check if close components overlap with 'component'
     overlapping_components = []
-
-    component_centers = [get_component_center(c) for c in components]
-    component_center = get_component_center(component)
-    tree = spatial.KDTree(component_centers)
-    close_component_center_indexes = tree.query_ball_point(
-                                        component_center,
-                                        get_component_size(component)*2
-                                     )
-    close_component_centers = [component_centers[i] for i in
-                               close_component_center_indexes]
-    #close_component_centers.remove(component_center)
-    close_components = construct_component_from_centers(close_component_centers,
-                                                        components)
-
     for close_component in close_components:
-        if overlap(close_component, component):
+
+        # skip the component itself
+        if close_component.index == component.index:
+            continue
+
+        if close_component.bounding_box.intersects(component.bounding_box):
             overlapping_components.append(close_component)
 
     return overlapping_components
 
-def overlap(componentA, componentB):
-    # begin = (x_begin, y_begin);
-    # end = (x_end, y_end)
-
-    x_begin_in_range = componentA.begin[0] in range(componentB.begin[0],
-                       componentB.end[0])
-    x_end_in_range = componentA.end[0] in range(componentB.begin[0],
-                     componentB.end[0])
-    y_begin_in_range = componentA.begin[1] in range(componentB.begin[1],
-                       componentB.end[1])
-    y_end_in_range = componentA.end[1] in range(componentB.begin[1],
-                     componentB.end[1])
-
-    condition1 = x_begin_in_range and y_end_in_range
-    condition2 = x_end_in_range and y_end_in_range
-    condition3 = x_begin_in_range and y_begin_in_range
-    condition4 = x_end_in_range and y_begin_in_range
-
-    return condition1 or condition2 or condition3 or condition4
-
 
 def construct_components(bounding_boxes):
 
-    components = []
-    # Take bounding boxes and convert into Component objects
-    for box_index, bounding_box in enumerate(bounding_boxes):
-        component = Component(bounding_box, box_index)
-        components.append(component)
-
-    return components
+    return [
+        Component(bounding_box, box_index)
+        for box_index, bounding_box in enumerate(bounding_boxes)
+    ]
 
 
-def get_component_center(component):
+def get_diagonal_length(component):
 
-    # Take component object and find its center
-    x = component.begin[0] + (component.end[0] - component.begin[0])/2
-    y = component.begin[1] + (component.end[1] - component.begin[1])/2
+    begin = component.bounding_box.begin
+    end = component.bounding_box.end
 
-    return (x, y)
+    diagonal_length_squared = sum((end - begin)**2)
 
-
-def get_component_size(component):
-
-    # Take component object and find its size
-    width = component.end[0]-component.begin[0]
-    height = component.end[1]-component.begin[1]
-    # assume box is always a square
-    return width
+    return math.sqrt(diagonal_length_squared)
 
 
-def construct_component_from_centers(component_centers, all_components):
-
-    # Create Component objects out of component centers
-    components = []
-    for component_center in component_centers:
-        for component in all_components:
-            if get_component_center(component) == component_center:
-                components.append(component)
-
-    return components
-
+# TODO: put into unit test
 if __name__ == "__main__":
-    bounding_boxes = np.load("bounding_boxes.npy")
-    overlaps = create_overlap_lookup_dict(bounding_boxes)
 
-    for index, overlapping_components in overlaps.items():
-        print(index, [c.index for c in overlapping_components])
+    bounding_boxes = np.load("bounding_boxes.npy")
+
+    bounding_boxes = [fg.Roi((x_b, y_b), (x_e-x_b, y_e-y_b))
+                      for x_b, x_e, y_b, y_e in bounding_boxes]
+
+    components = create_components(bounding_boxes)
+
+    for component in components:
+        print(f"component index: {component.index}")
+        print(f"component bounding box: {component.bounding_box}")
+        print(f'component shape: {component.bounding_box.shape}')
+        for overlapping_component in component.overlapping_components:
+            print(f"overlapping component index: {overlapping_component.index}")
+            print(f"overlapping component bounding box: {overlapping_component.bounding_box}")
+
