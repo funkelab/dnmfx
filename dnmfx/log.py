13cb543be195eeacaa0d68dabef0fdf69a14da73
Alicia Lu <lualicia88@gmail.com>
2022-07-17 16:14:14 -0400

    Add class IterationLog and AggregateLog

---
+++ dnmfx/log.py
@@ -0,0 +1,43 @@
+import numpy as np
+import jax.numpy as jnp
+from jax.tree_util import register_pytree_node_class
+
+
+@register_pytree_node_class
+class IterationLog():
+
+    """
+    Construct the iteration loss matrix with shape (k, T), where k is the number of
+    connected components, T the batch size, whose each element (e.g. x) is the
+    reconstruction loss of a sampled component in a given connected component at a
+    time frame.
+
+            t_0  t_1  .    .   .   t_T
+    cc_0 |   x                           |
+    cc_1 |                               |
+    .    |                               |
+    .    |                               |
+    .    |                               |
+    cc_k |                               |
+    """
+
+    def __init__(self, num_connected_components, batch_size):
+        self.iteration_loss = np.zeros((num_connected_components,
+                                        batch_size))
+
+    def tree_flatten(self):
+        children = (self.iteration_loss)
+        aux_data = None
+        return (children, aux_data)
+
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        return cls(*children)
+
+class AggregateLog():
+
+    def __init__(self, iteration_loss):
+        if aggregate_loss == None:
+            aggregate_loss = iteration_loss
+        else:
+            aggregate_loss = np.stack((aggregate_loss, iteration_loss))
