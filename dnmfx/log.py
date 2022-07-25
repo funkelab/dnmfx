95610250ca129bedec77f8c9397730e0a3026802
Alicia Lu <lualicia88@gmail.com>
2022-07-25 12:41:53 -0400

    Collect intermediate factorizations and gradients in logging

--- dnmfx/log.py
+++ dnmfx/log.py
@@ -3,9 +3,29 @@ class IterationLog():
     iteration.
     """
 
-    def __init__(self, iteration, loss):
+    def __init__(self,
+            iteration,
+            loss,
+            grad_H_logits=None,
+            grad_W_logits=None,
+            grad_B_logits=None,
+            H_logits=None,
+            W_logits=None,
+            B_logits=None,
+            x_hat_logits=None):
+
         self.iteration = iteration
         self.loss = loss
+        self.grad_H_logits = grad_H_logits
+        self.grad_W_logits = grad_W_logits
+        self.grad_B_logits = grad_B_logits
+        self.H_logits = H_logits
+        self.W_logits = W_logits
+        self.B_logits = B_logits
+        self.x_hat_logits = x_hat_logits
+
+        assert iteration is not None and loss is not None, \
+            "Both iteration and loss have to be logged"
 
 
 class Log():
@@ -18,6 +38,23 @@ class Log():
 
         self.iteration_logs = []
 
-    def add_loss(self, iteration, loss):
+    def add_loss(self,
+                 iteration,
+                 loss,
+                 H_gradient,
+                 W_gradient,
+                 B_gradient,
+                 H_logits,
+                 W_logits,
+                 B_logits,
+                 x_hat_logits):
 
-        self.iteration_logs.append(IterationLog(iteration, loss))
+        self.iteration_logs.append(IterationLog(iteration,
+                                                loss,
+                                                H_gradient,
+                                                W_gradient,
+                                                B_gradient,
+                                                H_logits,
+                                                W_logits,
+                                                B_logits,
+                                                x_hat_logits))
