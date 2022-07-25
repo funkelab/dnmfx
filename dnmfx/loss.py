a769b2d9d9ab8685919f380114e820c8838d38bc
Alicia Lu <lualicia88@gmail.com>
2022-07-25 15:45:46 -0400

    Switch to original loss function on 07/21

--- dnmfx/loss.py
+++ dnmfx/loss.py
@@ -16,29 +16,28 @@ def l2_loss(
     assert len(B_logits.shape) == 2
 
     # get the current estimate for what x would look like (i.e., x_hat)
-    x_hat, x_hat_logits, h_logits, w_logits, b_logits = get_x_hat(
-        H_logits,
-        W_logits,
-        B_logits,
-        component_description,
-        frame_indices)
+    x_hat = get_x_hat(
+            H_logits,
+            W_logits,
+            B_logits,
+            component_description,
+            frame_indices)
 
-    return (jnp.linalg.norm(x - x_hat), (x_hat, x_hat_logits, h_logits, w_logits, b_logits))
+    return jnp.linalg.norm(x - x_hat)
 
 
-l2_loss_grad = jax.value_and_grad(l2_loss, argnums=(0, 1, 2), has_aux=True)
+l2_loss_grad = jax.value_and_grad(l2_loss, argnums=(0, 1, 2))
 
 def get_x_hat(H_logits, W_logits, B_logits, component_description, frames):
 
     i = component_description.index
     bb_i = component_description.bounding_box
 
-    w_logits = W_logits[frames, i]
-    h_logits = H_logits[i]
-    b_logits = B_logits[i]
+    w = sigmoid(W_logits[frames, i])
+    h = sigmoid(H_logits[i])
+    b = sigmoid(B_logits[i])
 
-    x_hat_logits = jnp.outer(w_logits, h_logits).reshape(-1, *h_logits.shape) + \
-                            b_logits
+    x_hat = jnp.outer(w, h).reshape(-1, *h.shape) + b
 
     for overlap in component_description.overlapping_components:
 
@@ -55,16 +54,11 @@ def get_x_hat(H_logits, W_logits, B_logits, component_description, frames):
         H_logits = H_logits.reshape(-1, *bb_i.shape)
         B_logits = B_logits.reshape(-1, *bb_i.shape)
 
-        w_logits = W_logits[frames, j]
-        h_logits = H_logits[slices_j]
-        b_logits = B_logits[slices_j]
+        w = sigmoid(W_logits[frames, j])
+        h = sigmoid(H_logits[slices_j])
+        b = sigmoid(B_logits[slices_j])
 
-        x_hat_logits = x_hat_logits.reshape(-1, *bb_i.shape)
-        x_hat_logits = x_hat_logits.at[slices_i].add(
-                    jnp.outer(w_logits, h_logits).reshape(-1, *h_logits.shape) + \
-                    b_logits)
+        x_hat = x_hat.reshape(-1, *bb_i.shape)
+        x_hat = x_hat.at[slices_i].add(jnp.outer(w, h).reshape(-1, *h.shape) + b)
 
-    x_hat = sigmoid(x_hat_logits).reshape(-1, *bb_i.shape)
-
-    return x_hat, x_hat_logits, h_logits, w_logits, b_logits
-
+    return x_hat
