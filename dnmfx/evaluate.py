e20e9a72ca5453b159455389f41788cc30373c97
Alicia Lu <lualicia88@gmail.com>
2022-07-28 13:11:28 -0400

    Add docstring to the pairing function

--- dnmfx/evaluate.py
+++ dnmfx/evaluate.py
@@ -40,6 +40,55 @@ def evaluate(H, B, W, dataset):
     B = B.reshape(-1, *components[0].shape)
     W = W.reshape(traces.shape)
 
+    """
+    Pair each decomposed component and component background with its corresponding
+    ground truth by constructing a loss matrix, in which every element (e.g. x)
+    represents the L2 norm distance between the factorization results and ground
+    truth.
+
+    More specifically, the loss matrix is structured as follows (here `C` stands for
+    Component; `B` stands for component Background):
+
+            C_0   C_1   ...   C_k   B_0   B_1   ...     B_k
+    Ĉ_0  |   x                                           |
+    Ĉ_1  |                                               |
+    .    |                                               |
+    .    |                                               |
+    .    |                                               |
+    Ĉ_k  |                                               |
+    B̂_0  |                                               |
+    B̂_1  |                                               |
+    .    |                                               |
+    .    |                                               |
+    .    |                                               |
+    B̂_k  |                                               |
+
+    Args:
+
+        components (list of :class:`ComponentDescription`):
+            Each component stores a list of other components it overlaps with.
+
+        bounding_boxes (:class:`funlib.geometry.Roi`):
+            A list of bounding boxes of the components.
+
+        background (array-like, shape: `(image_size, image_size)`):
+            The background image in assembling the synthetic dataset.
+
+        H (array-like, shape `(k, w*h)`):
+
+            The factorized matrix that contains all decomposed components.
+
+        B (array-like, shape `(k, w*h)`):
+
+            The factorized matrix that contains the backgrounds of all decomposed
+            components.
+
+        W (array-like, shape `(t, k)`):
+
+            The factorized matrix that contains the traces of all decomposed
+            components.
+    """
+
     loss_matrix = np.zeros((k*2, k*2))
     for i in range(k*2):
         for j in range(k*2):
