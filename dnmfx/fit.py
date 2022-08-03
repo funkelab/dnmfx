12eebe453e8dd0481d123d7c4729ca35719f7e7c
Alicia Lu <lualicia88@gmail.com>
2022-08-03 17:30:27 -0400

    Rewrite fit to implement distributed nmfx

--- dnmfx/fit.py
+++ dnmfx/fit.py
@@ -1,25 +1,29 @@
-from .component_description import create_component_description
+from datetime import datetime
+import pickle
 from .optimize import dnmf
 from .parameters import Parameters
 from .io import read_dataset
+from .initialize import initialize_normal
 
 
 def fit(
-        data_path,
-        max_iteration,
-        min_loss,
-        batch_size,
-        step_size,
-        l1_weight,
-        random_seed=None,
-        log_every=1,
-        log_gradients=False):
+        dataset_path,
+        groups_path,
+        group_index,
+        max_iteration=10000,
+        min_loss=1e-4,
+        batch_size=10,
+        step_size=1e-1,
+        l1_weight=0,
+        log_every=100,
+        log_gradients=False,
+        random_seed=None):
     """Use distributed NMF to estimate the components and traces for a dataset
     given as a zarr container.
 
     Args:
 
-        data_path (string):
+        dataset_path (string):
             The path to the zarr container containing the dataset. Should have
             a `sequence` dataset of shape `(t, [[z,], y,] x)` and a
             `component_locations` dataset of shape `(n, 2, d)`, where `n` is
@@ -27,6 +31,15 @@ def fit(
             `component_locations` stores the begin and end of each component,
             i.e., `component_locations[1, 0, :]` is the begin of component `1`
             and `component_locations[1, 1, :]` is its end.
+
+        groups_path (string):
+            The path to the pickle file containing the groups of the dataset. Should
+            be a list of lists; the list length is the number of groups; each list
+            contains an uncertain number of objects :class: `ComponentDescription`
+            that are members within a group.
+
+        group_index (int):
+            The index indicating which group is to be fitted.
 
         max_iteration (int):
             The maximum number of iterations to optimize for.
@@ -44,17 +57,24 @@ def fit(
         l1_weight (float):
             The influence of the L1 regularizer on the components and traces.
 
+        log_every (int):
+
+            How often to print iteration statistics.
+
+        log_gradients (bool):
+
+            Whether to record gradients and factor matrices (i.e. H, B, W) after the
+            1st iteration.
+
         random_seed (int):
+
             A random seed for the initialization of `H` and `W`. If not given,
             a different random seed will be used each time.
 
-        log_every (int):
-            How often to print a log statement during optimization.
-
     Returns:
 
-        A dictionary with "H" (the estimated components), "W" (the estimated
-        traces), and "log" (statistics per iteration).
+        The maxtrix factors of the dataset (i.e. H, W, B) and the losses stored as
+        :class: `Log`.
     """
 
     parameters = Parameters()
@@ -63,17 +83,47 @@ def fit(
     parameters.batch_size = batch_size
     parameters.step_size = step_size
     parameters.l1_weight = l1_weight
+    parameters.log_every = log_every
+    parameters.log_gradients = log_gradients
+    parameters.random_seed = random_seed
 
-    dataset = read_dataset(data_path)
-    component_descriptions = \
-        create_component_description(dataset.bounding_boxes)
+    if parameters.random_seed is None:
+        parameters.random_seed = int(datetime.now().strftime("%Y%m%d%H%M%S"))
+    else:
+        parameters.random_seed = random_seed
+
+    with open(groups_path, "rb") as f:
+        content = f.read()
+    groups = pickle.loads(content)
+
+    component_descriptions = groups[group_index]
+
+    component_size = None
+    for description in component_descriptions:
+        size = description.bounding_box.get_size()
+        if component_size is not None:
+            assert component_size == size, \
+                    "Only components of the same size are supported for now"
+        else:
+            component_size = size
+
+    dataset = read_dataset(dataset_path)
+    sequence = dataset.sequence
+    num_frames = sequence.shape[0]
+    num_components = dataset.num_components
+
+    H_logits, W_logits, B_logits = initialize_normal(
+            num_components,
+            num_frames,
+            component_size,
+            parameters.random_seed)
 
     H, W, B, log = dnmf(
-        dataset.sequence,
-        component_descriptions,
-        parameters,
-        log_every,
-        log_gradients,
-        random_seed)
+            sequence,
+            component_descriptions,
+            parameters,
+            H_logits,
+            W_logits,
+            B_logits)
 
-    return {"H": H, "W": W, "B": B, "log": log}
+    return H, W, B, log
