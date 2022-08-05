f7985bec20d3d66980674bdb22f433797523ba06
Alicia Lu <lualicia88@gmail.com>
2022-08-05 16:53:30 -0400

    Remve function pair from evaluate; only returns l2 norm bw grd truth and reconstruction

--- dnmfx/evaluate.py
+++ dnmfx/evaluate.py
@@ -40,87 +40,9 @@ def evaluate(H, B, W, dataset):
     B = B.reshape(-1, *components[0].shape)
     W = W.reshape(traces.shape)
 
-    pairings, loss_matrix = pair(components, bounding_boxes, background, H, B, k)
-    mispairings = [(c, c_hat) for c, c_hat in pairings if c != c_hat]
+    component_loss = {i: np.linalg.norm(components[i, :] - H[i, :])
+                      for i in range(k)}
+    trace_loss = {i: np.linalg.norm(traces[i, :] - W[i, :])
+                      for i in range(k)}
 
-    component_loss = {i: loss_matrix[i , j] for i, j in pairings[:k]}
-    trace_loss = {i: np.linalg.norm(traces[i, :] - W[j, :])
-                  for i, j in pairings[:k]}
-
-    return mispairings, component_loss, trace_loss
-
-
-def pair(components,
-         bounding_boxes,
-         background,
-         H, B, k):
-
-    """
-    Pair each decomposed component and component background with its corresponding
-    ground truth by constructing a loss matrix, in which every element (e.g. x)
-    represents the L2 norm distance between the factorization results and ground
-    truth.
-
-    More specifically, the loss matrix is structured as follows (here `C` stands for
-    Component; `B` stands for component Background):
-
-            C_0   C_1   ...   C_k   B_0   B_1   ...     B_k
-    Ĉ_0  |   x                                           |
-    Ĉ_1  |                                               |
-    .    |                                               |
-    .    |                                               |
-    .    |                                               |
-    Ĉ_k  |                                               |
-    B̂_0  |                                               |
-    B̂_1  |                                               |
-    .    |                                               |
-    .    |                                               |
-    .    |                                               |
-    B̂_k  |                                               |
-
-    Args:
-
-        components (list of :class:`ComponentDescription`):
-            Each component stores a list of other components it overlaps with.
-
-        bounding_boxes (:class:`funlib.geometry.Roi`):
-            A list of bounding boxes of the components.
-
-        background (array-like, shape: `(image_size, image_size)`):
-            The background image in assembling the synthetic dataset.
-
-        H (array-like, shape `(k, w*h)`):
-
-            The factorized matrix that contains all decomposed components.
-
-        B (array-like, shape `(k, w*h)`):
-
-            The factorized matrix that contains the backgrounds of all decomposed
-            components.
-
-        W (array-like, shape `(t, k)`):
-
-            The factorized matrix that contains the traces of all decomposed
-            components.
-    """
-
-    loss_matrix = np.zeros((k*2, k*2))
-
-    for i in range(k*2):
-        for j in range(k*2):
-            if i < k:
-                component_a = H[i, :, :]
-            elif i >= k:
-                component_a = B[i-k, :, :]
-                component_slice = bounding_boxes[i-k].to_slices()
-            if j < k:
-                component_b = components[j, :, :]
-            elif j >= k:
-                component_slice = bounding_boxes[j-k].to_slices()
-                component_b = background[component_slice]
-            loss_matrix[i][j] = np.linalg.norm(component_a - component_b)
-
-    ground_truth_indices, reconstruction_indices = linear_sum_assignment(loss_matrix)
-    pairings = list(zip(ground_truth_indices, reconstruction_indices))
-
-    return pairings, loss_matrix
+    return component_loss, trace_loss
