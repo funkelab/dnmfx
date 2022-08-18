cdb3e9c920ed7b3d4e6fb79b5037522ca2a50607
Alicia Lu <lualicia88@gmail.com>
2022-08-18 04:29:22 -0400

    Compute and return reconstruction error

--- dnmfx/evaluate.py
+++ dnmfx/evaluate.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.optimize import linear_sum_assignment
 
 
-def evaluate(H, W, dataset):
+def evaluate(H, W, B, dataset):
     """Get the number of component and component background ID mismatches and
     find the reconstruction error per component and per trace.
 
@@ -34,13 +34,35 @@ def evaluate(H, W, dataset):
 
     components = dataset.components
     traces = dataset.traces
-    k = dataset.num_components
-    num_pixels = components.size
+    sequence = dataset.sequence
+    num_components = dataset.num_components
+    num_frames = dataset.num_frames
+    bounding_boxes = dataset.bounding_boxes
+    background = dataset.background
+    noises = dataset.noises
+    num_component_pixels = components.size
+    num_image_pixels = background.size
+
+    reconstruction = np.stack([background] * num_frames)
+    reconstruction += noises
+
+    for i in range(num_components):
+        component_slice = bounding_boxes[i].to_slices()
+        cell = W[i, :].reshape(-1, 1) @ H[i, :].reshape(1, -1) + B[i, :]
+        cell = cell.reshape(num_frames, *components[0].shape)
+        reconstruction[(slice(None),) + component_slice] = cell
+
     H = H.reshape(-1, *components[0].shape)
+    B = B.reshape(-1, *components[0].shape)
 
-    component_loss = {i: float(np.linalg.norm(components[i, :] - H[i, :])/num_pixels)
-                      for i in range(k)}
-    trace_loss = {i: float(np.linalg.norm(traces[i, :] - W[i, :])/num_pixels)
-                      for i in range(k)}
+    component_loss = {i: float(np.linalg.norm(
+                               components[i] - H[i])/num_component_pixels)
+                      for i in range(num_components)}
+    trace_loss = {i: float(np.linalg.norm(
+                           traces[i] - W[i])/num_image_pixels)
+                      for i in range(num_components)}
+    reconstruction_error = {t: float(np.linalg.norm(
+                    sequence[t, :, :] - reconstruction[t, :, :])/num_image_pixels)
+                    for t in range(num_frames)}
 
-    return component_loss, trace_loss
+    return component_loss, trace_loss, reconstruction_error
