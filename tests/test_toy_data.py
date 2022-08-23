67be35d0c7bf7fa4de09a66d2c6588a377189f14
kunyanglu <lualicia88@gmail.com>
2022-08-22 20:21:33 -0700

    Rewrite the interface of the first test case that checks for gradients after 1st iteration.

--- tests/test_toy_data.py
+++ tests/test_toy_data.py
@@ -1,43 +1,36 @@
-from dnmfx.evaluate import evaluate
-from dnmfx.utils import sigmoid
-from dnmfx.optimize import dnmf
+from .toy_dataset import Parameters as DatasetParameters
+from .toy_dataset import create_synthetic_dataset
 from dnmfx.component_description import create_component_description
+from dnmfx.evaluate import evaluate
+from dnmfx.fit import fit
+from dnmfx.optimize import dnmf
 from dnmfx.parameters import Parameters as DNMFXParameters
+from dnmfx.utils import sigmoid
 import jax.numpy as jnp
 import math
 import unittest
-from .toy_dataset import create_synthetic_dataset
-from .toy_dataset import Parameters as DatasetParameters
 
 
 class TestCaseA(unittest.TestCase):
 
     def test_case_A(self):
-
         """
         Checking gradient computation on the simplest test case.
         """
-        cell_centers = [(0.5, 0.5)]
-        image_size = 1
-        cell_size = 1
-        num_frames = 1
+
         toy_data = self._generate_toy_data(
-                           cell_centers,
-                           image_size,
-                           cell_size,
-                           num_frames)
+                           cell_centers= [(0.5, 0.5)],
+                           image_size=1,
+                           cell_size=1,
+                           num_frames=1)
 
-        max_iteration = 1
-        batch_size = 1
-        log_every=1
-        log_gradients = True
-        H, W, B, log = self._fit(toy_data,
-                                 max_iteration,
-                                 batch_size,
-                                 log_every,
-                                 log_gradients)
+        H, W, B, log = fit(toy_data,
+                           max_iteration=1,
+                           batch_size=1,
+                           log_every=2,
+                           log_gradients=True)
 
-        iter_log = log.iteration_logs[0]
+        iter_log = log[0].iteration_logs[0]
         w_logits = iter_log.W_logits[0][0]
         h_logits = iter_log.H_logits[0][0]
         b_logits = iter_log.B_logits[0][0]
@@ -107,23 +100,3 @@ class TestCaseA(unittest.TestCase):
         toy_data.sequence = toy_data.render()
 
         return toy_data
-
-
-    def _fit(self,
-            toy_data,
-            max_iteration,
-            batch_size,
-            log_every,
-            log_gradients):
-
-        component_descriptions = \
-                        create_component_description(toy_data.bounding_boxes)
-        dnmfx_parameters = DNMFXParameters(max_iteration=max_iteration,
-                                           batch_size=batch_size)
-        H, W, B, log = dnmf(toy_data.sequence,
-                            component_descriptions,
-                            dnmfx_parameters,
-                            log_every=log_every,
-                            log_gradients=log_gradients,
-                            random_seed=2001)
-        return H, W, B, log
